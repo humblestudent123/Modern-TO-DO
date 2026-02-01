d779dc1d2357b1b7540642ecb8aa4a2b2882f067
Bruh <vasickinfedor@gmail.com>
2026-02-01 11:45:29 +0300

    --

--- taskflow/src/widgets/Header/Header.tsx
+++ taskflow/src/widgets/Header/Header.tsx
@@ -3,7 +3,7 @@ import styles from "./Header.module.css";
 export default function Header() {
   return (
     <header className={styles.header}>
-      <h2>TaskFlow</h2>
+      <h1 className={styles.logo}>TaskFlow</h1>
     </header>
   );
 }
